3408234900ec8d54161b8b1b32bdbbe9082c9d06
J. Michael Brown <jmichaelbrown8@gmail.com>
2021-11-18 22:32:14 -0800

    Built the timer logic with some stub start and end game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
 // variables to store data
 
 // store the questions in an array of objects
-/*
-    {
-        question: "Choose the first alphabetical letter.",
-        choices: ["A", "B", "C", "D"],
-        answer: "A"
-    }
-*/
 const questions = [
     {
         question: "Choose the first alphabetical letter.",
@@ -36,12 +29,87 @@ var questionIndex = 0;
 
 // time left
 var timeLeft = 0;
+var intervalId;
 
 // score
 var score = 0;
 
 // scoreboard (in local storage)
 
+// nav toggle (to switch between game and scoreboard)
+const navToggleEl = document.querySelector("#nav-toggle");
+
+// time left
+const timeLeftEl = document.querySelector("#time-left");
+
+// score
+const scoreEl = document.querySelector("#score");
+
+// main element (where the messages and questions will display)
+const mainEl = document.querySelector("main");
+
+/**
+ * Creates and displays the welcome message and the start button in the main element.
+ */
+function displayStartMessage() {
+
+}
+
+/**
+ * Displays the current time in the time left element.
+ */
+function countdownAndDisplayTimeLeft() {
+    timeLeft--;
+    if (timeLeft <= 0) {
+        timeLeftEl.textContent = "BUZZZZ!!!!";
+        return endGame();
+    }
+    return displayTimeLeft();
+}
+
+/**
+ * Displays the current time in the time left element.
+ */
+function displayTimeLeft() {
+    timeLeftEl.textContent = "Time left: " + timeLeft + "s";
+}
+
+/**
+ * Starts the game by displaying the first question and starting the countdown timer.
+ */
+function startGame() {
+    timeLeft = 6;
+    displayTimeLeft();
+    startTimer();
+    
+}
+
+/**
+ * Ends the game by clearing the timer, and displaying the final message.
+ */
+function endGame() {
+    stopTimer();
+}
+
+/**
+ * Start the countdown timer.
+ */
+function startTimer() {
+    if (intervalId === undefined) {
+        intervalId = setInterval(countdownAndDisplayTimeLeft, 1000);
+    } else {
+        console.warn("Already have a timer running with id: " + intervalId);
+    }
+    
+}
+
+/**
+ * Stop the countdown timer. 
+ */
+function stopTimer() {
+    clearInterval(intervalId);
+    intervalId = undefined;
+}
 
 // What needs to be on the page:
 // welcome and description
